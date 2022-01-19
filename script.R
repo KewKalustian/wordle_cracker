9f4f0f67a8f98dda1c9da32c23cfe0c91143cd1c
Kework K. Kalustian <56342591+KewKalustian@users.noreply.github.com>
2022-01-19 11:30:04 +0100

    browser search results of bingo words

--- script.R
+++ script.R
@@ -41,3 +41,26 @@ bingo_words_options <- semi_join(suggestions, foo, by = "words")
 
 # result
 print(bingo_words_options)
+
+# browser search of the bingo words arranged in desc. order according to the most searched word (in bing)
+url <- sprintf(
+  "https://www.bing.com/search?q=%s", as.character(bingo_words_options$words)
+)
+
+browse_stats <- function(x){
+  
+ bingo_words <- x %>% 
+                substring(nchar(.)-4, nchar(.))
+
+ bing_browser_search_res <- html_text(html_nodes(read_html(x),".sb_count")) %>%
+                                str_remove_all("[:punct:]") %>% 
+                                str_remove_all("[:alpha:]") %>% 
+                                as.numeric()
+ 
+stats <- data.frame(bingo_words, bing_browser_search_res )
+return(stats)
+}  
+
+map_df(url, browse_stats) %>% 
+  arrange(desc(bing_browser_search_res))
+
