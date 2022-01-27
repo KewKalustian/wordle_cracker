56d7ce68660dd170d4a2ce8b034eabc3149cb0ce
Kework K. Kalustian <56342591+KewKalustian@users.noreply.github.com>
2022-01-27 11:45:43 +0100

    Create script.py

---
+++ script.py
@@ -0,0 +1,29 @@
+
+
+import pandas as pd
+import numpy as np
+import string as str
+
+# already used characters in previous guesses
+stop_ch = pd.DataFrame({"letters":[
+ 
+"t","i",
+"n","g",
+"s","m",
+"e",
+"i",
+"r"] 
+ })
+
+abc = pd.DataFrame({'letters': list(str.ascii_lowercase)})
+
+# saving all remaining characters into an object
+
+possible =  (
+    stop_ch.merge(abc, 
+              on='letters', 
+              how='outer', 
+              indicator=True)
+    .query('_merge != "both"')
+    .drop(columns='_merge')
+)
